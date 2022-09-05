442694a587e860fcb9fd223a668e4c3e795176bc
patthamawa <108643215+patthamawa@users.noreply.github.com>
2022-09-05 10:57:40 +0700

    add

---
+++ resources/views/layout/partias/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="">
+    <div class="container">
+    </div>
+</footer>
---
+++ resources/views/layout/partias/header.blade.php
@@ -0,0 +1,7 @@
+<section class="text-center">
+    <div class="container">
+        <h1>Demo</h1>
+        <p class="text-normal">say something</p>
+        <p><a href="" class="btn ">some url</a></p>
+    </div>
+</section>
