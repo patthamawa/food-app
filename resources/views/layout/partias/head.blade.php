9e08615554227933b1f08f18373f8d3d1078a618
patthamawa <108643215+patthamawa@users.noreply.github.com>
2022-09-05 10:18:37 +0700

    add

---
+++ resources/views/layout/partias/head.blade.php
@@ -0,0 +1,7 @@
+<meta chaeset="utf-8">
+<meta name="viewport" content="width-device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>
+    
+</title>
