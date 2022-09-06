5f5b118af8c2a3625fec9bde9705d355f6f7ad1c
patthamawa <108643215+patthamawa@users.noreply.github.com>
2022-09-06 16:23:02 +0700

    add

---
+++ resources/views/form.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.inputlayout')
+@section('content')
+@endsection
+Create student form 
+1. Name
+2. Surname
+3. brithdate
+4. faculty -> dropdown list
+button
+1. submit
+2. cancel
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
