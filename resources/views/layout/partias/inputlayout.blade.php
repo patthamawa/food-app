43f023b731133cd222f431aef6373aed963e1e2b
patthamawa <108643215+patthamawa@users.noreply.github.com>
2022-09-06 16:45:42 +0700

    add

--- resources/views/layout/partias/inputlayout.blade.php
+++ resources/views/layout/partias/inputlayout.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        @include('layout.partials.head')
+        
     </head>
     <body>
         @yield('content')
-        @include('layout.partials.footer')
-        @include('layout.partials.footer-scripts')
+        
     </body>
 </html>
